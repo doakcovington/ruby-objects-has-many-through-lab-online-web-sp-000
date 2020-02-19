18a9071426c7a235274b39cfa3a9c2a4b96dd2ac
Doak Covington <doakcovington@protonmail.com>
2020-02-19 04:15:02 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -16,3 +16,9 @@ class Patient
   def new_appointment(doctor, date)
     Appointment.new(doctor, self, date)
   end
+
+  def appointments
+    Appointment.all.select do |appointment|
+      appointment.patient == self
+    end
+  end
