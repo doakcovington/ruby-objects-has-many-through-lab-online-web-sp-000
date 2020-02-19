79cb329264fe25bee79f22051ecdbe71dd178a1a
Doak Covington <doakcovington@protonmail.com>
2020-02-19 04:17:46 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -3,7 +3,9 @@ class Appointment
   attr_accessor :patient, :doctor, :date
 
   def initialize(patient, doctor, date)
-
+    @patient = patient
+    @doctor = doctor
+    @date = date
   end
 
   @@all = []
